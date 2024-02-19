84ab536a0477beee818a1d6286b1ee9872b45ba8
Nataliia <nataliia.petrychuk@gmail.com>
2024-02-19 08:32:32 +0200

    first commit project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// let number = 5;
+// const letBorderWight = 1;
+
+// const answers = [];
+
+// answers[0] = prompt("Как Ваше имя?", "");
+// answers[1] = prompt("Как Ваше фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+let incr = 10;
+let decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+
