6aba37603f67433d79d3da531d61658cdac64fbe
Nataliia <nataliia.petrychuk@gmail.com>
2024-02-20 02:18:47 +0200

    practice #1

--- js/script.js
+++ js/script.js
@@ -9,17 +9,41 @@
 // answers[1] = prompt("Как Ваше фамилия?", "");
 // answers[2] = prompt("Сколько вам лет?", "");
 
-let incr = 10;
-let decr = 10;
+// let incr = 10;
+// let decr = 10;
 
-++incr;
---decr;
+// ++incr;
+// --decr;
 
-console.log(incr);
-console.log(decr);
+// console.log(incr);
+// console.log(decr);
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2*4 == 8);
+// console.log(2*4 == 8);
+
+const numberOfFilms = +prompt("Cколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors:{},
+	genres: [],
+	privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	  b = prompt("На сколько оцените его?", ""),
+	  c = prompt("Один из последних просмотренных фильмов?", ""),
+	  d = prompt("На сколько оцените его?", "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
 
 
