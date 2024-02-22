50304c6749519e189d1c59b9c6d12c73ee44e985
Nataliia <nataliia.petrychuk@gmail.com>
2024-02-22 08:47:47 +0200

    function convert currency

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+//конвертер валют 
+const usdCurr = 28;
+const eurCurr = 28;
+
+function convert (amount, curr){
+	console.com(curr * amount);
+}
+convert(500, usdCurr);
+convert(500, eurCurr);
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+/* eslint-disable semi */
+"use strict";
 
+let num = 20;
+function showFirstMessage(text) {
+	console.log(text);
+	num = 10;
+}
+
+showFirstMessage("Hello World!");
+console.log(num);
+
+// function calc(a, b) {
+// 	return (a + b);
+// }
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+
+function ret() {
+	let num = 50;
+	return num;
+}
+const anotherNumber = ret();
+console.log(anotherNumber);
+
+const logger = function() {
+	console.log("Hello")
+};
+logger();
+
+const calc = (a, b) => a + b;
 
 
